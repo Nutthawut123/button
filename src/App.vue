4e62f7d8904aab3626ae9ccda88fc9431b032046
Nutthawut123 <68680249+Nutthawut123@users.noreply.github.com>
2020-11-07 17:39:58 +0700

    button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+    <HelloWorld msg="You like avenger to tanos?" />
+    <br>
+    <div class="row">
+      <div class="col-12">
+      <img src="https://f.ptcdn.info/839/046/000/ofnpom5xdUDwFdo3Nei-o.jpg" width="200" height="300" />
+        <h1>{{total1}}</h1>
+        <div class="col-12">
+          <CsButton @cc="sum" /><br>
+        </div>
+      </div>
+      <div class="col-12">
+      <img src="https://i.pinimg.com/474x/6f/76/39/6f7639e90ffb39a08759dcc160a843d7.jpg" width="200" height="300" />
+        <h1>{{total2}}</h1>
+        <div class="col-12">
+          <CsButton @cc="sum1" />
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CsButton";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sum1(counterValue){
+    this.total2 = counterValue
+},
+},
+
+};
+
 </script>
 
 <style>
+body {
+      background-image: url(https://a-static.besthdwallpaper.com/ewnecch-rs-infinity-sngkhraamolokbnphuuenhlangkrancch-wx-ll-pepexr-2560x1600-1756_7.jpg);
+      background-size: auto ;
+    }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Londrina Solid', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  margin-top: 70px;
 }
 </style>
